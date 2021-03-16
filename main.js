73b55f2bc108debf097158eec78439067037c41b
hyksm <hiyoko3m.tech@gmail.com>
2021-03-16 15:47:20 +0900

    Add the validation for input row and column numbers

--- main.js
+++ main.js
@@ -10,8 +10,24 @@ const app = Vue.createApp({
     },
     methods: {
         reset_matrix() {
+            // Set row_num and column_num correctly
+            // and reflect them back to input_row_num and input_column_num.
             this.row_num = parseInt(this.input_row_num);
+            if (this.row_num <= 0) {
+                this.row_num = 1;
+            }
+            this.input_row_num = this.row_num;
+
             this.column_num = parseInt(this.input_column_num);
+            if (this.column_num <= 0) {
+                this.column_num = 1;
+            }
+            this.input_column_num = this.column_num;
+
+            // Initialize the matrix as the identity matrix
+            // if row_num and column_num coincide. Otherwise
+            // initialize as a near identity matrix
+            // not to become the sum of each row and column as 0.
             this.matrix = Array(this.row_num);
             for (let i = 0; i < this.matrix.length; i++) {
                 this.matrix[i] = Array(this.column_num).fill(0);
@@ -24,12 +40,14 @@ const app = Vue.createApp({
         row_scaling() {
             this.matrix = this.matrix.map(
                 row => {
+                    // The `s` is the sum of each row.
                     const s = row.reduce((sum, value) => sum + value, 0);
                     return row.map(x => x / s);
                 }
             )
         },
         column_scaling() {
+            // The `s` is the array having the sum of each column.
             const s = this.matrix.reduce(
                 (sum, row) => sum.map((x, i) => x + row[i]),
                 Array(this.matrix[0].length).fill(0)
