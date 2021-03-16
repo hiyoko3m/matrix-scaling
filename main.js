5c20ab709cf5fe314d43cbbe3317fa273db55d33
hyksm <hiyoko3m.tech@gmail.com>
2021-03-16 15:26:40 +0900

    Add reset_matrix function

--- main.js
+++ main.js
@@ -2,13 +2,24 @@ const app = Vue.createApp({
     data() {
         return {
             row_num: 3,
+            input_row_num: "3",
             column_num: 3,
+            input_column_num: "3",
             matrix: [[1, 1, 1], [0, 1, 0], [0, 0, 1]],
         }
     },
     methods: {
-        create_matrix() {
-            this.matrix = [[1, 0], [0, 1]]
+        reset_matrix() {
+            this.row_num = parseInt(this.input_row_num);
+            this.column_num = parseInt(this.input_column_num);
+            this.matrix = Array(this.row_num);
+            for (let i = 0; i < this.matrix.length; i++) {
+                this.matrix[i] = Array(this.column_num).fill(0);
+                this.matrix[i][Math.min(i, this.column_num - 1)] = 1;
+            }
+            for (let i = this.row_num; i < this.column_num; i++) {
+                this.matrix[this.row_num - 1][i] = 1;
+            }
         },
         row_scaling() {
             this.matrix = this.matrix.map(
