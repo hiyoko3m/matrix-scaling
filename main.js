bcb3fb1c091167b45e48212b7acce5706a3883e8
hyksm <hiyoko3m.tech@gmail.com>
2021-03-16 12:38:33 +0900

    Implement row_scaling and column_scaling

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
         return {
             row_num: 3,
             column_num: 3,
-            matrix: [[1, 0, 0], [0, 1, 0], [0, 0, 1]],
+            matrix: [[1, 1, 1], [0, 1, 0], [0, 0, 1]],
         }
     },
     methods: {
@@ -11,8 +11,21 @@ const app = Vue.createApp({
             this.matrix = [[1, 0], [0, 1]]
         },
         row_scaling() {
+            this.matrix = this.matrix.map(
+                row => {
+                    const s = row.reduce((sum, value) => sum + value, 0);
+                    return row.map(x => x / s);
+                }
+            )
         },
         column_scaling() {
+            const s = this.matrix.reduce(
+                (sum, row) => sum.map((x, i) => x + row[i]),
+                Array(this.matrix[0].length).fill(0)
+            );
+            this.matrix = this.matrix.map(
+                row => row.map((value, i) => value / s[i])
+            );
         },
     },
 })
